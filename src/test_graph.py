75eebe23cbb290bc979da3ce357e42a996dc5a85
george <george@george-ThinkPad-X220.(none)>
2012-11-14 23:46:38 +0000

    added some sanity checking in the visualization library

---
+++ src/test_graph.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+'''
+    This is a just a test for the visualization functions i developed
+'''
+
+from plot_graph import render_graph
+from plot_graph import create_graph
+import numpy as np
+
+a = np.array([[ 0,  0,  1],
+              [ 0,  1,  0],
+              [ 1,  0,  0]])
+
+nodes = ["Creta","Lefkada","Cyprus"]
+try:    
+	test_graph = create_graph(nodes, a)
+	render_graph(test_graph, "test_graph")
+except Exception as inst:
+	print inst.args[0]
