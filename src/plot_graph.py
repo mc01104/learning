818744fa80a84c8e16a5197f0b2b1764b93da98c
GeorgeF <georgios.fagogenis@gmail.com>
2012-11-15 12:29:43 +0000

    initial

---
+++ src/plot_graph.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# This program adds up integers in the command line
+"""
+	This file implements visualization functions for graphs using pygraph.
+	It is probably going to become obsolete when I start use the full learning package
+"""
+
+print "ante kai gamisou"
