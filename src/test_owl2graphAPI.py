a48909452d0570624533c22ff237f79c276bac09
GeorgeF <georgios.fagogenis@gmail.com>
2012-11-15 16:27:40 +0000

    extract subgraph from ontology and store it as an image - works-> needs more testing

---
+++ src/test_owl2graphAPI.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+'''
+    This is a just a test for the owl2graphAPI
+'''
+
+from owl2graphAPI import *
+from plot_graph import render_graph
+from plot_graph import create_graph
+import numpy as np
+import rospy
+
+rospy.init_node('test_owl2graphAPI')
+
+#query for objects that are connected with specific properties
+solutions = list(query_property("eats"))
+
+#get the labels for the graph
+labels = get_labels(solutions)
+print "---- labels -----" 
+print labels
+
+#compute the adjacency_list
+adj_list = compute_adjacency_list(solutions)
+print "---- adjacency_list ----"
+print adj_list
+
+#compute the adjacency matrix
+adj_mtr = compute_adjacency_matrix(labels,solutions)
+print "---- adjacency_matrix ----"
+print adj_mtr
+
+#testing the final (actual client function)
+g = Graph_bundle()
+g = extract_graph("eats")
+print "---- Graph Bundle ----"
+print "Graph_bundle:Labels ="
+print g.labels
+print "Graph_bundle: Adjacency Matrix ="
+print g.adjacency_matrix
+
+#save the graph as an image
+g_plot = create_graph(g.labels,g.adjacency_matrix)
+render_graph(g_plot, "g_graph")
