34fcf413747cd068ea4fd22a5f295f615fcc994e
GeorgeF <georgios.fagogenis@gmail.com>
2012-11-27 18:33:21 +0000

    test

--- src/learn_bn.py
+++ src/learn_bn.py
@@ -5,6 +5,9 @@
 '''
 from pebl import data, result
 from pebl.learner import *
+from pebl_interface import graph2pebl
+from pebl_interface import render_pebl_graph
+import numpy as np
 
 #TODO: this should take as arguments
 #      data  : txt file
@@ -12,12 +15,18 @@ from pebl.learner import *
 #	   conf  : configuration file - type of learnerm number of learners
 
 if __name__ == "__main__":
-		dataset = data.fromfile('/home/george/Desktop/diabetes_pebl_train.txt')
-		dataset.discretize([0,1,2,3,4,5,6,7,8],[],5)
-		#~ learner = greedy.GreedyLearner(dataset, max_iterations = 10000)
-		#~ results = learner.run()
-		#~ results.tohtml("results2")
-		learners = [ greedy.GreedyLearner(dataset, max_iterations=1000000) for i in range(5) ] + \
-		[ simanneal.SimulatedAnnealingLearner(dataset) for i in range(5) ]
-		merged_result = result.merge([learner.run() for learner in learners])
-		merged_result.tohtml("example3-result")
+		dataset = data.fromfile('/home/george/Desktop/grades_train.txt')
+		dataset.discretize(numbins=5)
+		learner = greedy.GreedyLearner(dataset, max_iterations = 10000)
+		nodes_list = ["SAT","Difficulty","Intelligence","Grade","Letter"]
+		adjacency_matrix = np.array([[0,0,0,0,0],[0,0,0,1,0],[1,0,0,1,0],[0,0,0,0,1],[0,0,0,0,0]])
+		g = graph2pebl(nodes_list, adjacency_matrix)
+		learner.seed = g
+		render_pebl_graph(learner.seed,"initial")
+		results = learner.run()
+		results.tohtml("results6")
+
+		#~ learners = [ greedy.GreedyLearner(dataset, max_iterations=1000000) for i in range(5) ] + \
+		#~ [ simanneal.SimulatedAnnealingLearner(dataset) for i in range(5) ]
+		#~ merged_result = result.merge([learner.run() for learner in learners])
+		#~ merged_result.tohtml("example4-result")
