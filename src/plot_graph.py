59e8dd3433a5e3b5d2a0b2e4e0159561a6e5a337
GeorgeF <georgios.fagogenis@gmail.com>
2012-12-11 10:57:30 +0000

    added functionality for legend creation and support for multi-graphs. Now I need to modify the graph classes to store info for the label

--- src/plot_graph.py
+++ src/plot_graph.py
@@ -14,7 +14,7 @@ sys.path.append('/usr/lib/graphviz/python/')
 sys.path.append('/usr/lib/pyshared/python2.7')
 import gv
 import numpy as np
-
+from copy import copy, deepcopy
 
 # Import pygraph
 from pygraph.classes.graph import graph
@@ -86,7 +86,7 @@ def render_graph_color(graph_object,filename_str):
 	graph.write_png(name)
 
 #TODO: merge this function with the above
-#TODO: implement mixed graph, the two opposing arrows don't look nice!
+#TODO: implement mixed graph, the two opposing arrows don't look nice! --- DONE
 #TODO: if this module ends-up being one function -> move it to graph_util	
 #TODO: also display multiple properties on edges.
 def render_multi_prop_graph(multi_prop_graph,filename_str):
@@ -101,9 +101,10 @@ def render_multi_prop_graph(multi_prop_graph,filename_str):
 			 
 	if is_directed(adjacency_matrix):
 		graph = pydot.Dot(graph_type='digraph')
+		direction = 'forward'
 	else:
 		graph = pydot.Dot(graph_type='graph',compound='true',mindist='0',ranksep='0',nodesep='0')
-	
+		direction = 'none'
 	
 	nodes_dot=[]
 
@@ -112,28 +113,34 @@ def render_multi_prop_graph(multi_prop_graph,filename_str):
 		nodes_dot.append(tmp)
 		graph.add_node(tmp)
 
-	for (i,j) in get_indices(adjacency_matrix):
-				graph.add_edge(pydot.Edge(nodes_dot[i], nodes_dot[j], label=multi_prop_graph.property_str, labelfontcolor="#009933", fontsize="10.0", color=multi_prop_graph.edge_colormap[node_list[i],node_list[j]]))
-	
+	y = deepcopy(adjacency_matrix)
+
+
+	for (i,j) in get_indices(y):
+		if y[i][j] == 1:
+			if y[j][i] ==1:
+				y[j][i] =0
+				direction = 'none'
+			graph.add_edge(pydot.Edge(nodes_dot[i], nodes_dot[j], label=multi_prop_graph.property_str, labelfontcolor="#009933", fontsize="10.0", dir=direction,color=multi_prop_graph.edge_colormap[node_list[i],node_list[j]]))
+		if is_directed(adjacency_matrix) :
+			direction = 'forward'
+			
 	name =  "%s.png" % filename_str
 	
-	#add_legend(graph, multi_prop_graph.property_colormap)
+	#graph = add_legend(graph, property_colormap)
 	graph.write_png(name)
 
-		
+#tested		
 def add_legend(dot_graph_object,property_colormap):
 	legend = pydot.Cluster(graph_name = 'legend', label = 'Color', rankdir="TB")
 	node_list = []
-	for key,value in property_colormap.items():
-		node_list.append(pydot.Node(name=value, label=value,style='filled',shape='box',color=key)
-
-	[legend.add_node(n) for n in node_list]
+	for (key,value) in property_colormap.items():
+		node_list.append(pydot.Node(name=value, label=value,style='filled',shape='box',color=key))
+	
+	[legend.add_node(n) for n in node_list] 
 
 	for i in range(len(node_list) - 1):
 		legend.add_edge(pydot.Edge(node_list[i], node_list[i + 1],style="invis",constraint='True'))
 
 	dot_graph_object.add_subgraph(legend)
 	return dot_graph_object
-		
-		
-		
